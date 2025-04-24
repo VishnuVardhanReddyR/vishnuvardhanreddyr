556d229ea2ee5613ce7b201e46a567e1a66ecc42
VishnuVardhanReddyR <vishnurapuru10@gmail.com>
2025-04-24 15:09:26 -0400

    image addition
    inserted image

--- components/NavComponents/LandingPage.js
+++ components/NavComponents/LandingPage.js
@@ -20,7 +20,7 @@ function LandingPage({intro}) {
     <section ref={observe} className="bg-fixed bg-no-repeat bg-cover bg-macblack-m lg:bg-macblack">
       <div className="relative items-center justify-center w-full h-screen">
         <ParticlesAnime />
-        <IntroPage title={intro[0].title} typings={intro[0].typingTitle} />
+        <IntroPage title={intro[0].title} image={urlFor(intro[0].image).url()} typings={intro[0].typingTitle} />
       </div>
     </section>
   )
